73fd5f202b464e08ac2a239870d66bfa72953112
Sergey Lebedev <graf@tp.(none)>
2012-03-03 00:04:02 +0400

    arguments positions changed, location value added

--- draw.py
+++ draw.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from patterns import *
 
-DIRECTION = {'n': (-1, 0),
-            'e': (0, 1),
-            's': (1, 0),
-            'w': (0, -1)}
+DIRECTIONS = {'n': (-1, 0),
+             'e': (0, 1),
+             's': (1, 0),
+             'w': (0, -1)}
+
+AMOUNT_OF_WALLS = 20
+AMOUNT_OF_PLAYERS = (2, 4)
 
 pattern = compact
 #pattern = box_drawing
 pattern = classic
 width = 11
-height = 11
+height = 9
 width_aspect = 4
 height_aspect = 2
 wall_length = 2
@@ -36,13 +39,13 @@ for i in range(height_aspect*height + 1):
 #players
 x = 0
 y = 0
-players = [None]*height
+players = [None]*width
 for i in range(len(players)):
-    players[i] = [0]*width
+    players[i] = [0]*height
 
-player_list = [{'id': 1, 'x': x, 'y': y}]
+player_list = [{'id': 1, 'location': (x, y)}]
 for player in player_list:
-    players[y][x] =  player['id']      
+    players[x][y] =  player['id']      
 
 player_positions = width_aspect - 1
 player_pic = ['player']*player_positions
@@ -54,26 +57,26 @@ if cutoff != 0:
 #walls
 X = 1
 Y = 1
-walls = [None]*height
+walls = [None]*width
 for i in range(len(walls)):
-    walls[i] = [0]*width
+    walls[i] = [0]*height
 
 #wall_list = [{'type': 'vertical', 'x': X, 'y': Y}]
-wall_list = [{'type': 'horizontal', 'x': X, 'y': Y}]
+wall_list = [{'type': 'horizontal', 'location': (X, Y)}]
 for wall in wall_list:
-    walls[y][x] =  1
+    walls[x][y] =  1
 
 #adjacency_list
 adjacency_list = {}
-for i in range(height):
-    for j in range(width):
+for i in range(width):
+    for j in range(height):
         link_list = set([])
-        for direction in DIRECTION:
-            (row, col) = DIRECTION[direction]
-            if ((row + i) >= 0) & ((row + i) < height) &\
-               ((col + j) >= 0) & ((col + j) < width): 
-                link_list.add((row + i)*width + (col + j))
-        adjacency_list.update({i*width + j: link_list})  
+        for direction in DIRECTIONS:
+            (row, col) = DIRECTIONS[direction]
+            if ((col + j) >= 0) & ((col + j) < height) &\
+               ((row + i) >= 0) & ((row + i) < width): 
+                link_list.add((row + i, col + j))
+        adjacency_list.update({(i, j): link_list})
 
 vertical_wall = ['heavy_vertical']*(height_aspect*wall_length - 1)
 horizontal_wall = ['heavy_horizontal']*(width_aspect*wall_length - 1)
@@ -88,17 +91,19 @@ while True:
     for lines in field:
         temp_field.append(lines[:])
     
-    for player in player_list:   
+    for player in player_list:
+        (row, col) = player['location']   
         for i in range(len(player_pic)):
-            temp_field[player['y']*height_aspect + 1][player['x']*width_aspect + 1 + i] = player_pic[i]
+            temp_field[col*height_aspect + 1][row*width_aspect + 1 + i] = player_pic[i]
     
     for wall in wall_list:
+        (row, col) = wall['location']
         if wall['type'] == 'vertical':
             for i in range(len(vertical_wall)):
-                temp_field[(wall['y'] - 1)*height_aspect + 1 + i][wall['x']*width_aspect] = vertical_wall[i]
+                temp_field[(col - 1)*height_aspect + 1 + i][row*width_aspect] = vertical_wall[i]
         elif wall['type'] == 'horizontal':
             for i in range(len(horizontal_wall)):
-                temp_field[wall['y']*height_aspect][(wall['x'] - 1)*width_aspect + 1 + i] = horizontal_wall[i]
+                temp_field[col*height_aspect][(row - 1)*width_aspect + 1 + i] = horizontal_wall[i]
         else:
             pass  
 
@@ -116,10 +121,11 @@ while True:
     #available_positions = adjacency_list.copy()
     #print available_positions
     for wall in wall_list:
-        left_top = (wall['y'] - 1)*width + wall['x'] - 1 
-        right_top = (wall['y'] - 1)*width + wall['x']
-        left_bottom = wall['y']*width + wall['x'] - 1
-        right_bottom = wall['y']*width + wall['x']
+        (row, col) = wall['location']
+        left_top = (row - 1, col - 1)  
+        right_top = (row, col - 1) 
+        left_bottom = (row - 1, col) 
+        right_bottom = (row, col) 
 
         #print left_top, right_top,left_bottom, right_bottom
         if (wall['type'] == 'horizontal'):
@@ -134,73 +140,74 @@ while True:
             available_positions[right_bottom].difference_update(set([left_bottom]))
 
     command = raw_input()
+    loc = (x, y)
     if (command == 'i'):
-        if ((y-1)*width + x) in available_positions[y*width + x]:
-            players[y][x] = 0
+        if (x, y - 1) in available_positions[loc]:
+            players[x][y] = 0
             y -= 1
             y = max(0, y)
-            player_list[0]['y'] = y
-            players[y][x] = 1  
+            player_list[0]['location'] = (x, y)
+            players[x][y] = 1  
     elif (command == 'j'):
-        if (y*width + x - 1) in available_positions[y*width + x]:
-            players[y][x] = 0    
+        if (x - 1, y) in available_positions[loc]:
+            players[x][y] = 0    
             x -= 1
             x = max(0, x)
-            player_list[0]['x'] = x
-            players[y][x] = 1 
+            player_list[0]['location'] = (x, y)
+            players[x][y] = 1 
     elif (command == 'k'):
-        if ((y+1)*width + x) in available_positions[y*width + x]:
-            players[y][x] = 0    
+        if (x, y + 1) in available_positions[loc]:
+            players[x][y] = 0    
             y += 1
             y = min(height-1, y)
-            player_list[0]['y'] = y
-            players[y][x] = 1 
+            player_list[0]['location'] = (x, y)
+            players[x][y] = 1 
     elif (command == 'l'):
-        if (y*width + x + 1) in available_positions[y*width + x]:
-            players[y][x] = 0    
+        if (x + 1, y) in available_positions[loc]:
+            players[x][y] = 0    
             x += 1
             x = min(width-1, x)
-            player_list[0]['x'] = x
-            players[y][x] = 1
+            player_list[0]['location'] = (x, y)
+            players[x][y] = 1
     elif (command == 'I'):
-        walls[Y][X] = 0    
+        walls[X][Y] = 0    
         Y -= 1
         Y = max(1, Y)
         if (wall_list[0]['type'] == 'vertical') & (Y <= 1):
             wall_list[0]['type'] = 'horizontal'
-        wall_list[0]['y'] = Y
-        walls[Y][X] = 1 
+        wall_list[0]['location'] = (X, Y)
+        walls[X][Y] = 1 
     elif (command == 'J'):
-        walls[Y][X] = 0    
+        walls[X][Y] = 0    
         X -= 1
         X = max(1, X)
         if (wall_list[0]['type'] == 'horizontal') & (X <= 1):
             wall_list[0]['type'] = 'vertical'
-        wall_list[0]['x'] = X
-        walls[Y][X] = 1 
+        wall_list[0]['location'] = (X, Y)
+        walls[X][Y] = 1 
     elif (command == 'K'):
-        walls[Y][X] = 0    
+        walls[X][Y] = 0    
         Y += 1
         Y = min(height - wall_length + 1, Y)
         if (wall_list[0]['type'] == 'vertical') & (Y > height - wall_length):
             wall_list[0]['type'] = 'horizontal'
-        wall_list[0]['y'] = Y
-        walls[Y][X] = 1 
+        wall_list[0]['location'] = (X, Y)
+        walls[X][Y] = 1 
     elif (command == 'L'):
-        walls[Y][X] = 0    
+        walls[X][Y] = 0    
         X += 1
         X = min(width - wall_length + 1, X)
         if (wall_list[0]['type'] == 'horizontal') & (X > width - wall_length):
             wall_list[0]['type'] = 'vertical'
-        wall_list[0]['x'] = X
-        walls[Y][X] = 1
+        wall_list[0]['location'] = (X, Y)
+        walls[X][Y] = 1
     elif (command == 'R'):
         if (wall_list[0]['type'] == 'horizontal'):
             wall_list[0]['type'] = 'vertical'   
         elif (wall_list[0]['type'] == 'vertical'):
             wall_list[0]['type'] = 'horizontal'       
     elif (command == 'N'):
-        wall_list.insert(0, {'type': 'horizontal', 'x': 1, 'y': 1})
+        wall_list.insert(0, {'type': 'horizontal', 'location': (1, 1)})
             
     elif (command == 'q'):
         break
