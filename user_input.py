a8095176dcebecc34fbe9b6d42432216fe4546dd
Sergey Lebedev <sergey.lebedev@inbox.ru>
2012-03-16 10:05:00 +0400

    added switch to user input

--- user_input.py
+++ user_input.py
@@ -31,17 +31,13 @@ def w2p(wall_list):
 def user_turn(player_list, player, wall_list, available_positions, players):
     (x, y) = player['location']
     loc = (x, y)
-    walls_installed = 0
+
     #print available_positions[loc]
     neighbors = []
     for location in available_positions[loc]:
         neighbors.append(location)
 
     p = w2p(wall_list)
-    draw(player_list, wall_list, neighbors)
-    ready = True
-    command = raw_input()
-    first_symbol = False
     #(X, Y) = wall['location']
     command_list = []
     command_dict = {}
@@ -50,98 +46,113 @@ def user_turn(player_list, player, wall_list, available_positions, players):
         command_list.append(char)
         command_dict[char] = i
 
-    numbers[char] = char        
-    if command in command_list:
-        players[x][y] = 0
-        (x, y) = neighbors[command_dict[command]]
-        player['location'] = (x, y)
-        players[x][y] = 1  
-    else:
-        ready = False
-        first_symbol = True
-
-    if player['amount_of_walls'] == 0:
-        ready = True
-
-    while (not ready) :
-        #if first_symbol: 
-        command = raw_input()
-        #    first_symbol = False
-        if (command == 'i'):
-            if walls_installed != 0:
-                wall = wall_list[len(wall_list) - 1]    
-                Y -= 1
-                Y = max(1, Y)
-                if (wall['type'] == 'vertical') & (Y <= 1):
-                    wall['type'] = 'horizontal'
-                wall['location'] = (X, Y)
- 
-        elif (command == 'j'):
-            if walls_installed != 0:
-                wall = wall_list[len(wall_list) - 1]   
-                X -= 1
-                X = max(1, X)
-                if (wall['type'] == 'horizontal') & (X <= 1):
-                    wall['type'] = 'vertical'
-                wall['location'] = (X, Y)
-
-        elif (command == 'k'):
-            if walls_installed != 0:
-                wall = wall_list[len(wall_list) - 1]  
-                Y += 1
-                Y = min(height - wall_length + 1, Y)
-                if (wall['type'] == 'vertical') & (Y > height - wall_length):
-                    wall['type'] = 'horizontal'
-                wall['location'] = (X, Y)
-
-        elif (command == 'l'):
-            if walls_installed != 0:
-                wall = wall_list[len(wall_list) - 1]  
-                X += 1
-                X = min(width - wall_length + 1, X)
-                if (wall['type'] == 'horizontal') & (X > width - wall_length):
-                    wall['type'] = 'vertical'
-                wall['location'] = (X, Y)
-
-        elif (command == 'r'):
-            if walls_installed != 0:
-                wall = wall_list[len(wall_list) - 1]
-                if (wall['type'] == 'horizontal'):
-                    wall['type'] = 'vertical'   
-                elif (wall['type'] == 'vertical'):
-                    wall['type'] = 'horizontal'   
-    
-        elif (command == 'n'):
-            if (walls_installed == 0) and (player['amount_of_walls'] != 0):
-                wall = None
-                for i in range(1, width):
-                    for j in range(1, height):
-                        if p[(i, j)] != set([]):           
-                            wall_type = list(p[(i, j)])
-                            wall = {'type': wall_type[0], 'location': (i, j)}
-                            break
-                if wall != None:
-                    wall_list.append(wall)
-                    walls_installed +=1
-                    (X, Y) = wall['location']
-
-        elif (command == 'd'):
-            if walls_installed != 0:
-                removed_wall = wall_list.pop()
-                (X, Y) = removed_wall['location']
-                walls_installed -=1
-        elif (command == 'b'):
-            if walls_installed != 0:
-                if wall['type'] in p[(X, Y)]:
-                    ready = True                  
+    numbers[char] = char      
+    ready = False
+    second_stage = False
+    while not ready:  
+        while not ready and not second_stage:
+            draw(player_list, wall_list, neighbors)
+            command = raw_input()
+            if command in command_list:
+                players[x][y] = 0
+                (x, y) = neighbors[command_dict[command]]
+                player['location'] = (x, y)
+                players[x][y] = 1
+                ready = True
             else:
                 ready = False
-            player['amount_of_walls'] -= walls_installed    
-        #elif (command == 'q'):
-        #    end = True
-        #elif (command == 'q!'):
-        #    end = True
-        #    win = True
-        else:
-            pass
-        draw(player_list, wall_list)
+                second_stage = True
+
+            if player['amount_of_walls'] == 0:
+                ready = True
+
+        walls_installed = 0
+        while not ready and second_stage:
+            #if first_symbol: 
+            command = raw_input()
+            #    first_symbol = False
+            if (command == 'i'):
+                if walls_installed != 0:
+                    wall = wall_list[len(wall_list) - 1]    
+                    Y -= 1
+                    Y = max(1, Y)
+                    if (wall['type'] == 'vertical') & (Y <= 1):
+                        wall['type'] = 'horizontal'
+                    wall['location'] = (X, Y)
+     
+            elif (command == 'j'):
+                if walls_installed != 0:
+                    wall = wall_list[len(wall_list) - 1]   
+                    X -= 1
+                    X = max(1, X)
+                    if (wall['type'] == 'horizontal') & (X <= 1):
+                        wall['type'] = 'vertical'
+                    wall['location'] = (X, Y)
+
+            elif (command == 'k'):
+                if walls_installed != 0:
+                    wall = wall_list[len(wall_list) - 1]  
+                    Y += 1
+                    Y = min(height - wall_length + 1, Y)
+                    if (wall['type'] == 'vertical') & (Y > height - wall_length):
+                        wall['type'] = 'horizontal'
+                    wall['location'] = (X, Y)
+
+            elif (command == 'l'):
+                if walls_installed != 0:
+                    wall = wall_list[len(wall_list) - 1]  
+                    X += 1
+                    X = min(width - wall_length + 1, X)
+                    if (wall['type'] == 'horizontal') & (X > width - wall_length):
+                        wall['type'] = 'vertical'
+                    wall['location'] = (X, Y)
+
+            elif (command == 'r'):
+                if walls_installed != 0:
+                    wall = wall_list[len(wall_list) - 1]
+                    if (wall['type'] == 'horizontal'):
+                        wall['type'] = 'vertical'   
+                    elif (wall['type'] == 'vertical'):
+                        wall['type'] = 'horizontal'   
+        
+            elif (command == 'n'):
+                if (walls_installed == 0) and (player['amount_of_walls'] != 0):
+                    wall = None
+                    for i in range(1, width):
+                        for j in range(1, height):
+                            if p[(i, j)] != set([]):           
+                                wall_type = list(p[(i, j)])
+                                wall = {'type': wall_type[0], 'location': (i, j)}
+                                break
+                    if wall != None:
+                        wall_list.append(wall)
+                        walls_installed +=1
+                        (X, Y) = wall['location']
+
+            elif (command == 'd'):
+                if walls_installed != 0:
+                    removed_wall = wall_list.pop()
+                    (X, Y) = removed_wall['location']
+                    walls_installed -=1
+            elif (command == 'b'):
+                if walls_installed != 0:
+                    if wall['type'] in p[(X, Y)]:
+                        ready = True                  
+                else:
+                    ready = False
+                player['amount_of_walls'] -= walls_installed    
+            #elif (command == 'q'):
+            #    end = True
+            #elif (command == 'q!'):
+            #    end = True
+            #    win = True
+            elif (command == 's'):
+                if walls_installed != 0:
+                    removed_wall = wall_list.pop()
+                    (X, Y) = removed_wall['location']
+                    walls_installed -=1
+                ready = False
+                second_stage = False
+            else:
+                pass
+            draw(player_list, wall_list)
