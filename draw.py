ba689672c2bb5298fa4126b3e405452f2bc51655
Sergey Lebedev <sergey.lebedev@inbox.ru>
2012-03-27 09:20:29 +0400

    default color description for each char added

--- draw.py
+++ draw.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from patterns import *
 from settings import *
-import curses
 import locale
 
 locale.setlocale(locale.LC_ALL,"")
 code = locale.getpreferredencoding()
 
 enable_curses = True
+if enable_curses:
+    import curses   
 
 #pattern = compact
 #pattern = box_drawing
@@ -31,7 +32,7 @@ for i in range(height_aspect*height + 1):
             else:
                 char = 'light_vertical_and_horizontal'
 
-        field[i].append(char)
+        field[i].append({'char': char, 'color': 'default'})
 
 #colorwalls
 invertor = ''
@@ -89,24 +90,24 @@ def corner_polish(field):
             horizontal_min = 0
             horizontal_max = len(field[i]) - 1
             horizontal_minimax = [horizontal_min, horizontal_max]
-            char = field[i][j]
+            char = field[i][j]['char']
             if (char == 'light_vertical_and_horizontal'):
                 if (i == vertical_min) and (j not in horizontal_minimax):                    
-                    field[i][j] = 'light_down_and_horizontal'
+                    field[i][j]['char'] = 'light_down_and_horizontal'
                 if (i == vertical_max) and (j not in horizontal_minimax):
-                    field[i][j] = 'light_up_and_horizontal'
+                    field[i][j]['char'] = 'light_up_and_horizontal'
                 if (j == horizontal_min) and (i not in vertical_minimax):
-                    field[i][j] = 'light_vertical_and_right'
+                    field[i][j]['char'] = 'light_vertical_and_right'
                 if (j == horizontal_max) and (i not in vertical_minimax):
-                    field[i][j] = 'light_vertical_and_left'
+                    field[i][j]['char'] = 'light_vertical_and_left'
                 if (i == vertical_min) and (j == horizontal_min):   
-                    field[i][j] = 'light_down_and_right'
+                    field[i][j]['char'] = 'light_down_and_right'
                 elif (i == vertical_min) and (j == horizontal_max):
-                    field[i][j] = 'light_down_and_left'
+                    field[i][j]['char'] = 'light_down_and_left'
                 elif (i == vertical_max) and (j == horizontal_max):
-                    field[i][j] = 'light_up_and_left'                
+                    field[i][j]['char'] = 'light_up_and_left'                
                 elif (i == vertical_max) and (j == horizontal_min):
-                    field[i][j] = 'light_up_and_right'                 
+                    field[i][j]['char'] = 'light_up_and_right'                 
 
 corner_polish(field)
 
@@ -126,14 +127,16 @@ def info(player_list):
 
 def draw(player_list, wall_list, curscr, additional=[]):
     temp_field = []
-    for lines in field:
-        temp_field.append(lines[:])
+    for i in range(len(field)):
+        temp_field.append([])
+        for j in range(len(field[i])):
+            temp_field[i].append(field[i][j].copy())
     
     i = 0
     for player in player_list:
         (row, col) = player['location']   
         for j in range(len(player_pic[i])):
-            temp_field[col*height_aspect + 1][row*width_aspect + 1 + j] = player_pic[i][j]
+            temp_field[col*height_aspect + 1][row*width_aspect + 1 + j]['char'] = player_pic[i][j]
         i +=1    
 
     for wall in wall_list:
@@ -144,13 +147,13 @@ def draw(player_list, wall_list, curscr, additional=[]):
             if pattern == colorwalls:               
                 vertical_wall_template = '%s_%s'%(color_template, vertical_wall_template)    
             for i in range(len(vertical_wall)):
-                temp_field[(col - 1)*height_aspect + 1 + i][row*width_aspect] = vertical_wall_template
+                temp_field[(col - 1)*height_aspect + 1 + i][row*width_aspect]['char'] = vertical_wall_template
         elif wall['type'] == 'horizontal':
             horizontal_wall_template = 'heavy_horizontal'
             if pattern == colorwalls:               
                 horizontal_wall_template = '%s_%s'%(color_template, horizontal_wall_template)
             for i in range(len(horizontal_wall)):
-                temp_field[col*height_aspect][(row - 1)*width_aspect + 1 + i] = horizontal_wall_template
+                temp_field[col*height_aspect][(row - 1)*width_aspect + 1 + i]['char'] = horizontal_wall_template
         else:
             pass  
 
@@ -158,7 +161,7 @@ def draw(player_list, wall_list, curscr, additional=[]):
         (row, col) = additional[i]  
         digit[digit_positions/2] = str(i + 1)
         for j in range(digit_positions):
-            temp_field[col*height_aspect + 1][row*width_aspect + 1 + j] = digit[j]  
+            temp_field[col*height_aspect + 1][row*width_aspect + 1 + j]['char'] = digit[j]  
 
     if enable_curses:
         [MAX_Y, MAX_X] = curscr.getmaxyx()
@@ -172,7 +175,7 @@ def draw(player_list, wall_list, curscr, additional=[]):
         for i in range(height_aspect*height + 1):
             string = ''
             for j in range(width_aspect*width + 1):
-                    string += pattern[temp_field[i][j]]
+                    string += pattern[temp_field[i][j]['char']]
             try:
                 curscr.move(cur_y + 1, horizontal_offset)
                 curscr.addstr(string.encode(code))
@@ -196,7 +199,7 @@ def draw(player_list, wall_list, curscr, additional=[]):
         for i in range(height_aspect*height + 1):
             string = ' '*horizontal_offset
             for j in range(width_aspect*width + 1):
-                    string += pattern[temp_field[i][j]]
+                    string += pattern[temp_field[i][j]['char']]
             print string
         info_string = info(player_list)
         print ' '*horizontal_offset + info_string
